685bcd4a7ea9b2d9e5bf4632cf5b8c57494f21f2
MeliaWijana <meliaa.wijana@gmail.com>
2025-08-14 00:15:52 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,36 +1,20 @@
-import os
-import requests
-from dotenv import load_dotenv
+import transformers
+import torch
 
-load_dotenv()
+model_id = "aisingapore/Llama-SEA-LION-v3.5-8B-R"
 
-API_KEY = os.getenv("SEA_LION_API_KEY")
-BASE_URL = "https://api.sea-lion.ai/v1"
+pipeline = transformers.pipeline(
+    "text-generation",
+    model=model_id,
+    model_kwargs={"torch_dtype": torch.bfloat16},
+    device_map="auto",
+)
+messages = [
+    {"role": "user", "content": "Apa sentimen dari kalimat berikut ini?\nKalimat: Buku ini sangat membosankan.\nJawaban: "},
+]
 
-HEADERS = {
-    "Authorization": f"Bearer {API_KEY}",
-    "Content-Type": "application/json",
-    "accept": "text/plain"
-}
-
-def list_models():
-    url = f"{BASE_URL}/models"
-    response = requests.get(url, headers=HEADERS)
-    response.raise_for_status()
-    return response.json()  # returns list of models
-
-def chat_with_sealion(prompt, model="aisingapore/Llama-SEA-LION-v3-70B-IT", max_tokens=10):
-    url = f"{BASE_URL}/chat/completions"
-    payload = {
-        "model": model,
-        "max_completion_tokens": max_tokens,
-        "messages": [
-            {"role": "user", "content": prompt}
-        ]
-    }
-    response = requests.post(url, headers=HEADERS, json=payload)
-    response.raise_for_status()
-    return response.text.strip()
-    outputs = model.generate(**inputs, max_new_tokens=max_new_tokens)
-    reply = tokenizer.decode(outputs[0, inputs["input_ids"].shape[1]:], skip_special_tokens=True)
-    return reply.strip()
+outputs = pipeline(
+    messages,
+    max_new_tokens=256,
+)
+print(outputs[0]["generated_text"][-1])

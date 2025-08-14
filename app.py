9dd8d43b76724a9db53fcea0e435254b49161dd2
MeliaWijana <meliaa.wijana@gmail.com>
2025-08-15 00:26:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,6 @@ with open("index.html") as f:
     html_content = f.read()
     st.markdown(html_content, unsafe_allow_html=True)
 
-# --- Baca file HTML ---
-with open("index.html", "r", encoding="utf-8") as f:
-    html_code = f.read()
-
 # --- Tampilkan HTML di Streamlit ---
 components.html(html_code, height=1200, scrolling=True)
 

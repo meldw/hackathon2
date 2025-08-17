75076a56060c81e0bbd3262c489c72bd35d0f1fb
MeliaWijana <meliaa.wijana@gmail.com>
2025-08-17 22:24:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import streamlit as st
 from model import generate_response
 import streamlit.components.v1 as components
 
-with open("index.html") as f:
-    html_content = f.read()
-    st.markdown(html_content, unsafe_allow_html=True)
+# --- Baca file HTML ---
+with open("index.html", "r", encoding="utf-8") as f:
+    html_code = f.read()
 
 
 # --- Tampilkan HTML di Streamlit ---
